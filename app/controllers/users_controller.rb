ae64533e6bad6f5602bcba29901baf78c3e58a10
EC2 Default User <ec2-user@ip-172-31-37-152.ec2.internal>
2022-02-21 07:06:07 +0000

    Podsフォルダをgit管理対象から除外

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,18 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  
   def show
     @user = User.find(params[:id])
   end
+  
   def edit
     @user = User.find(params[:id])
     if @user != current_user
         redirect_to user_path(current_user), alert: "不正なアクセスです。"
     end
   end
+  
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
